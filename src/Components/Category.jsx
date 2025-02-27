585a22856bfb47daa04db1e3aaf78bf8e0111296
unknown <kapkotikartik69@gmail.com>
2025-02-27 23:10:20 +0530

    initial commit

---
+++ src/Components/Category.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import CategoryObj from "../CategoryObj";
+
+function Category({ filter }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleClick(category) {
+    setSelectedCategory(category);
+    filter(category);
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-6 w-full pt-5">
+      {CategoryObj.map((item) => (
+        <div
+          key={item.id}
+          className={`w-[100px] h-[100px] rounded-md shadow-lg flex flex-col items-center justify-center cursor-pointer transition-all duration-200 active:scale-90 
+          ${
+            selectedCategory === item.name
+              ? "bg-green-400 text-white" 
+              : "bg-white hover:bg-green-300 text-gray-900"
+          }`}
+          onClick={() => handleClick(item.name)}
+        >
+          <div className="font-semibold flex flex-col items-center justify-center p-2">
+            {item.icon}
+            <span className="mt-1">{item.name}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Category;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Nav from "../Components/Nav";
+import Category from "../Components/Category";
+import Card from "../Components/Card";
+import { food_items } from "../data/food";
+
+const Home = () => {
+  let [Cate, setCate] = useState(food_items);
+
+  function filter(category) {
+    if (category === "All") {
+      setCate(food_items);
+    } else {
+      let newList = food_items.filter((item) => item.food_category === category);
+      setCate(newList);
+    }
+  }
+
+  return (
+    <div className="bg-slate-100 min-h-screen">
+      <Nav />
+      <Category filter={filter} />
+      <div className="w-full flex flex-wrap gap-3 justify-center items-center pt-5 pb-10">
+        {Cate.map((item) => (
+          <Card
+            key={item.id}
+            name={item.food_name}
+            image={item.food_image}
+            price={item.price}
+            id={item.id}
+            type={item.food_type}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
